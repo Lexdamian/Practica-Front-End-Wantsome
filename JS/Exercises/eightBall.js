839897c1c858d3689778cb5157f8d101be3965e4
Lexdamian <damian.alecsandru@gmail.com>
2019-11-01 16:37:33 +0200

    8ball fortune console game

---
+++ JS/Exercises/eightBall.js
@@ -0,0 +1,42 @@
+let userName = "Dan";
+
+userName ? console.log("Hello " + userName) : console.log("Hello");
+
+let userQuestion = "";
+console.log("How will be your future " + userName + "?");
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0: 
+    eightBall = "It is certain";
+    break;
+  case 1: 
+    eightBall = "It is decidedly so";
+    break;
+  case 2: 
+    eightBall = "Reply hazy try again";
+    break;
+  case 3: 
+    eightBall = "Cannot predict now";
+    break;
+  case 4: 
+    eightBall = "Do not count on it";
+    break;
+  case 5: 
+    eightBall = "My sources say no";
+    break;
+  case 6: 
+    eightBall = "Outlok not so good";
+    break;
+  case 7: 
+    eightBall = "Signs point to yes";
+    break;
+  default:
+    eightBall = "Not sure what to say"
+    break;
+}
+
+console.log(eightBall);
