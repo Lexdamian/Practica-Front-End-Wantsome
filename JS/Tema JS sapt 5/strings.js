75fcba898f16ea9810e78262c0fd64e4ecc585c6
LexDamian <damian.alecsandru@gmail.com>
2019-11-11 09:27:08 +0200

    Modificari conform given comments

--- JS/Tema JS sapt 5/strings.js
+++ JS/Tema JS sapt 5/strings.js
@@ -1,23 +1,25 @@
 // exercitiul 1
 function isStringOrNumber(verification) {
-    return typeof verification === 'string' || typeof verification === 'number';
+    return typeof verification === 'string';
 }
 
-console.log(isStringOrNumber('w3resources'));
-console.log(isStringOrNumber(2));
+// console.log(isStringOrNumber('w3resources'));
+// console.log(isStringOrNumber(2));
 
 // exercitiul 2
 function isBlank(blank) {
-    return (!blank || 0 === blank.lenght);
+    return (!blank || 0 === blank.length);
 }
 
-console.log(isBlank(''));
+// console.log(isBlank(''));
+
+
 
 // exercitiul 3
 function stringToArray(strToArray) {
     return strToArray.split(' ');
 }
-console.log(stringToArray("This function transforms strings to arrays"));
+// console.log(stringToArray("This function transforms strings to arrays"));
 
 //exercitiul 4
 function abbrevName(abreviation) {
@@ -27,52 +29,58 @@ function abbrevName(abreviation) {
     return firstName.concat(" ", firstLetter.toUpperCase(), ".");
 }
 
-console.log(abbrevName("Alexandru Damian"));
+// console.log(abbrevName("Alexandru Damian"));
+
+
 
 // exercitiul 5
 function capitalize(firstLetter) {
     return firstLetter[0].toUpperCase() + firstLetter.slice(1);
 }
 
-console.log(capitalize("romania"));
+// console.log(capitalize("romania"));
+
 
 
 // exercitiul 6
-function truncateString(myInput) {
-    return myInput.substr(0, 4);
+function truncateString(myInput, byHowMany) {
+    return myInput.substr(0, byHowMany);
 }
-console.log(truncateString("enciclipedie"));
+// console.log(truncateString("enciclopedie", 6));
 
 // exercitiul 7 
-function isUpperCaseAt(upperCaseInput) {
-    return upperCaseInput.charCodeAt(4) <= 90 && upperCaseInput.charCodeAt(4) >= 60;
+function isUpperCaseAt(upperCaseInput, wichOne) {
+    return upperCaseInput.charCodeAt(wichOne) <= 90 && upperCaseInput.charCodeAt(wichOne) >= 60;
 }
-console.log(isUpperCaseAt("RomaNia este o tara"));
+// console.log(isUpperCaseAt("RomaNia este o tara", 7));
+
+
 
 // exercitiul 8
-function strInsert(strInsert) {
-    return strInsert.slice(0, 3) + " suntem " + strInsert.slice(4);
+function strInsert(str, strToBeInserted, atIndex) {
+    return str.slice(0, atIndex) + strToBeInserted + " " + str.slice(atIndex);
 }
-console.log(strInsert("Noi destepti"));
+// console.log(strInsert("Noi destepti", "Andrei", 7));
+
 
 
 // exercitiul 9
-function removeFirstOfElement(element) {
-    return element.replace("the", "");
+function removeFirstOccurence(element, toBeRemoved) {
+    return element.replace(toBeRemoved, "");
 }
 
-console.log(removeFirstOfElement("The quick brown fox jumps over the lazy dog."));
+console.log(removeFirstOccurence("The quick brown fox jumps over the lazy dog.", 'the'));
 
 
 // exercitiul 10
 function compareStrings(str1, str2) {
     return str1.toUpperCase() == str2.toUpperCase();
 }
-console.log(compareStrings("abcd", "AbcD"));
+// console.log(compareStrings("abcd", "AbcD"));
 
 
 // exercitiul 11
 function uncapitalize(firstCharacter) {
     return firstCharacter[0].toLowerCase() + firstCharacter.slice(1);
 }
-console.log(uncapitalize("Javascript course"));
+// console.log(uncapitalize("Javascript course"));
